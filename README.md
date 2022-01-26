f38d857004699bcfbc61fdff5b92a1f2d97c7e7c
Michel <vilbertmichel9@gmail.com>
2022-01-26 04:34:41 +0100

    premierequestion

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # TP_POO
+
+## Expliquez les concepts POO suivants en expliquant dans quel cas nous pouvons utiliser ces éléments et comment techniquement ils s’utilisent : 
+
+# L’héritage
+
+# L’interface
+
+# La méthode abstraite
+
+# La classe abstraite
+
+# Les propriété privée / protected
+# Les méthodes statiques
--- tp.php
+++ tp.php
@@ -1,1 +1,30 @@
-test
+<?php 
+
+/* TP PHP ORIENTE OBJET*/
+
+// Expliquez les concepts POO suivants en expliquant dans quel cas nous pouvons utiliser ces éléments et comment techniquement ils s’utilisent : 
+
+/* L'héritage */
+
+// C'est lorsqu'on apporte les propriétés d'une classe à une autre, on l'utilise avec extends
+
+exemple:
+
+class Parents {
+    $yeux;
+    $cheveux;
+}
+
+class Enfants extends Parents { La class Enfants hérite des propriétés de parents
+    "$yeux";
+    "$cheveux";
+}
+
+
+/* # L’interface */
+
+
+
+
+
+?>
