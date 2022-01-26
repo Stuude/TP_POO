d9ca84f94a5172590f8731251d9456c821a7aa13
Michel <vilbertmichel9@gmail.com>
2022-01-26 04:51:49 +0100

    tpfini

--- tp.php
+++ tp.php
@@ -21,8 +21,46 @@ class Enfants extends Parents { La class Enfants hérite des propriétés de par
 }
 
 
-/* # L’interface */
+/* L’interface */
 
+// L'interface est un ensemble de fonction qu'on définit et qu'on veut appliquer dans une class + on est obligé de mettre les fonctions dedans
+
+exemple:
+
+interface Monstre {
+    function Griffe();
+    function Morsure();
+}
+
+class Gobelin implements Monstre {
+    $Vol;
+
+    function Griffe(){
+    }
+    function Morsure(){
+    }
+}
+// vu qu'on a ajouté l'interface dans notre classe on est obligé d'utiliser les fonctions présentes dans celle ci 
+
+/* La méthode abstraite */
+
+// C'est lorsqu'on marque une méthode avec celle ci 
+
+/* Les propriété privée / protected */
+
+// Propriété privée
+
+// La propriété privée est lorsque qu'on définit des propriétés
+// des classes qui restent dans la class sans modification, 
+// cela permet aussi de proteger sa class contre toute modification exterieure
+
+// Propriété protected 
+
+// Les propriétés protégés sont limités a leurs propres classes
+
+/* Les méthodes statiques */
+
+// le fait de déclarer une méthode en méthode statique nous permettra de l'utiliser sans instancier une class
 
 
 
